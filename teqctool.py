4fc1e28078b11f7562e721e9ff3f191b8bec4374
Aslak Grinsted <ag+github@glaciology.net>
2018-09-07 09:21:12 +0200

    Make splitting bat files instead of splitting

--- teqctool.py
+++ teqctool.py
@@ -40,7 +40,11 @@ def get_meta(input_file):
     meta['final date & time'] = parse(meta['final date & time'])
     
     meta['file size (bytes)'] = int(meta['file size (bytes)'])
-    meta['sample interval'] = float(meta['sample interval'])
+    try:
+        meta['sample interval'] = float(meta['sample interval'])
+    except ValueError:
+        meta['sample interval'] = 0.0
+        
     meta['possible missing epochs'] = int(meta['possible missing epochs'])
     meta['antenna latitude (deg)'] = float(meta['antenna latitude (deg)'])
     meta['antenna longitude (deg)'] = float(meta['antenna longitude (deg)'])
@@ -59,74 +63,20 @@ def get_meta(input_file):
     return meta
 
 
-def convert_and_split(input_file, min_time_delta=datetime.timedelta(minutes=5), join_small_gaps=datetime.timedelta(minutes=5)):
-    
-    rinexfiles=[]
-    
-    meta = get_meta(input_file)
-    starts = meta['survey starts']
-    unit = settings.units[meta['receiver ID number']]
-    
-    #combine starts if they are less than xx min appart. 
-    dt = np.diff(starts)
-    eliminate = (dt[0:-1]<join_small_gaps).nonzero()
-    starts = np.delete(starts, np.add(eliminate,1))
-    
-    for (ix,start) in enumerate(starts[:-1]):
-        end = starts[ix+1]-datetime.timedelta(seconds=10)
-        if (end-start<min_time_delta):
-            print('skipping short survey...')
-            continue 
-        
-        outputfolder = os.path.join(settings.folders['rinex'],
-                                  str(start.year),
-                                  unit)
-        
-        pathlib.Path(outputfolder).mkdir(parents=True, exist_ok=True)
-        
-        if (end-start.replace(second=0,hour=0,minute=0)<datetime.timedelta(days=1)):
-            #if it does not span midnight:
-            outputname = '{}_{:%Y-%m-%d_%H%M}'.format(unit,start)
-            outputname = os.path.join(outputfolder,outputname)
-            obsfile = outputname + '.{:%y}o'.format(start)
-            navfile = outputname + '.{:%y}n'.format(start)
-            command=[teqc,
-                     '-leica','mdb',
-                     '-st', '{:%Y_%m_%d:%H:%M:%S.%f}'.format(start), 
-                     '-e', '{:%Y_%m_%d:%H:%M:%S.%f}'.format(end),
-                     '+obs', obsfile,
-                     '+nav', navfile,
-                     input_file]
-            if os.stat(obsfile).st_size==0: 
-                os.remove(obsfile)
-                os.remove(navfile)
-#            else:
-#            m = get_meta(obsfile)
-#            print(m)
-#            return
-            print(' -> {} dt={}'.format(obsfile,end-start))
-        else: 
-            outputname = '{}_{:%Y%m%d%H%M}_bin_'.format(unit,start)
-            outputname = os.path.join(outputfolder,outputname)
-            command=[teqc,
-                     '-leica','mdb',
-                     '-st', '{:%Y_%m_%d:%H:%M:%S.%f}'.format(start), 
-                     '-e', '{:%Y_%m_%d:%H:%M:%S.%f}'.format(end),
-                     '-tr','d',
-                     '+obs', '+','+nav', '+','-tbin','1d',
-                     outputname,
-                     input_file]
-            print(' -> ' + outputname + 'ddd0.YYo')
-#        print(' '.join(command))
-        print(subprocess.check_output(command))
+
+
+
 
 
 if __name__ == '__main__':
    
-    q =  get_meta(r'originaldata\GPS1 180801\DBX\Default_8632_0731_190123.m00')
+    q =  get_meta(r'..\2018\GPS1 180801\DBX\Default_8632_0731_190123.m00')
+#    q  = get_meta(r'..\2015\CardNo3\DBX\testdtu3_6213_0327_102404.m00')
     print(q)
-    convert_and_split(r'originaldata\GPS1 180801\DBX\Default_8632_0731_190123.m00')
-
+    
+    q = get_meta(r'..\2018\GPS10 180801\Rinex\unit10_EG-N-012_2080.18o')
+    print(q)
+    
     
 
 
