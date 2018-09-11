73e77863bed34cf63eaaf2702237cf8d8c82861d
Aslak Grinsted <ag+github@glaciology.net>
2018-09-11 11:24:31 +0200

    Extract results

--- parseoutput.py
+++ parseoutput.py
@@ -13,6 +13,10 @@ import io
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob
+import settings
+import datetime
+import os
 
 def read_glab_output(inputfile):
     with open(inputfile, 'r') as file:
@@ -21,12 +25,19 @@ def read_glab_output(inputfile):
     errors = re.findall('^ERROR.*$',contents,re.MULTILINE)
     if errors:
         raise(Exception('gLAB error','\n'.join(errors)))
+        
+        
+    #station name 
+#    INFO INPUT Station marker: EG-C-200
+    stationname = re.search('^INFO INPUT Station marker: (.*)$',contents,re.MULTILINE)
+    if stationname: 
+        stationname = stationname.group(1)
+    else:
+        stationname = ''    
 
     #grep OUTPUT rows
     outputlines = re.findall('^OUTPUT.*$',contents,re.MULTILINE)
     outputlines = io.StringIO('\n'.join(outputlines))
-
-
 
     columnnames = ['rowtype',
                 'Year', 'DOY', 'Seconds of day',
@@ -44,18 +55,57 @@ def read_glab_output(inputfile):
 
     data = pd.read_table(outputlines, names=columnnames,
                          header = None, comment=r'#',delim_whitespace=True)
-    return data
+    data['datetime'] = pd.to_datetime({'year': data['Year'], 'month': 1, 'day': 1, 'second': data['Seconds of day']}) + pd.to_timedelta(data['DOY']-1,'D')
+    return (stationname,data)
+
+
+
+def make_table_of_all():
+    rootdir = settings.folders['output']
+    pattern = '*.glab'
+    
+    D = pd.DataFrame(columns=['id', 'datetime','lat','lon','z','sigmaE', 'sigmaN', 'sigmaZ'])
+    
+    for filename in glob.iglob(os.path.join(rootdir,'**',pattern), recursive=True):
+        print('Input file: {}'.format(filename))
+        (stationname,data) = read_glab_output(filename)
+        if stationname.lower().find('kinematic')>=0:
+            continue
+        if data.size<2:
+            continue
+#        dt = np.diff(data['datetime']) / np.timedelta64(1, 's')
+#        ix = dt.ravel().nonzero()
+#        end_of_forward_ix = ix[-1]+1
+        
+        avg_time = np.mean(data['datetime'] - datetime.datetime(2000,1,1))+datetime.datetime(2000,1,1)
+
+        row = data.iloc[-1]
+        
+        D.loc[D.shape[0]] = [stationname, avg_time, 
+                         row['latitude'], row['longitude'], row['height'],
+                         row['X error'], row['Y error'], row['Z error']
+                         ]
+    writer = pd.ExcelWriter(os.path.join(rootdir,'summary.xlsx'))
+    D.to_excel(writer,'Summary', index = False)
+    writer.save()
+        
+        
+    return D
+
+            
+        
+
+
 
 
 if __name__ == '__main__':
    
-    inputfile = r'C:\Users\ag\Documents\GitHub\GPSprocess\output\2017\unit1\unit1_2017-07-31_1216.txt'
-    inputfile = r'C:\Users\ag\Documents\GitHub\GPSprocess\output\2018\unit1\unit1_2018-07-31_1357.txt'
+    D=make_table_of_all()
     
-    inputfile = r'output\2017\unit8\unit8_2017-07-28_1202.txt'
+    inputfile = r'C:\Users\ag\HugeData\EGRIP GPS\GPSprocess\output\2017\unit3\EG-C-200_20170804_1705.txt'
     
     # inputfile = r'C:\Users\ag\Documents\GitHub\GPSprocess\output\2017\unit1\unit1_2017-07-26_1139.txt'
-    data=read_glab_output(inputfile)
+    (stationname,data) = read_glab_output(inputfile)
     
     d=data.iloc[-1]
     print(inputfile,d['latitude'],d['longitude'],d['height'])
